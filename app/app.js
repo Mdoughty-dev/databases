fb6c9fdddd9bb86a96c2c51eb3879fdf6330205d
mattpatii123 <maddodoughty@live.co.uk>
2026-02-24 09:21:33 +0000

    Add cors module to app.js

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const apiRouter = require("./routes/api-router");
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", express.static("public"));
